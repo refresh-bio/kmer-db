aae5fa23316150c7cecbf9d7f2f575bf1a3f8255
Sebastian Deorowicz <sdeorowicz@gmail.com>
2017-11-16 22:19:42 +0100

    Ograniczenie liczby wątków Radulsa do 12 (brzydkie, ale lepsze niż pozwolenie na tworzenie ich dowolnie wielu)

--- kmer_db/parallel_sorter.cpp
+++ kmer_db/parallel_sorter.cpp
@@ -14,6 +14,7 @@
 void ParallelSort(pair<pattern_id_t, pattern_id_t*> *arr, size_t arr_size, pair<pattern_id_t, pattern_id_t*> *tmp, int rec_size, int key_size, int n_threads)
 {
 #ifdef USE_RADULS
+	n_threads = std::max(n_threads, 12);
 	raduls::RadixSortMSD(reinterpret_cast<uint8_t*>(arr), reinterpret_cast<uint8_t*>(tmp), arr_size, rec_size, key_size, n_threads);
 #else
 	auto pid_comparer = [](const std::pair<pattern_id_t, pattern_id_t*>& a, const std::pair<pattern_id_t, pattern_id_t*>& b)->bool {
